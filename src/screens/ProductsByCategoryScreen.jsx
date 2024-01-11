ac770b0ef4402c7799d2c779d94caa284dfe7dca
Sebastian Toledo <69781552+Sebastian-Toledo@users.noreply.github.com>
2024-01-11 00:44:07 -0300

    clase 6 y 7

---
+++ src/screens/ProductsByCategoryScreen.jsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const ProductsByCategoryScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Productos por categoría</Text>
+    </View>
+  );
+};
+
+export default ProductsByCategoryScreen;
+
+const styles = StyleSheet.create({});
