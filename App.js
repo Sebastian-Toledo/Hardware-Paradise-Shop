ad7d9eada3e7c0bbf51d7af7696d5990078960ba
Sebastian Toledo <69781552+Sebastian-Toledo@users.noreply.github.com>
2024-01-14 13:51:27 -0300

    primera entrega

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
-import CategoriesScreen from "./src/screens/CategoriesScreen";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <View style={styles.container}>
+      <Text>Hola, Coder!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
